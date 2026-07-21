16efe25e88a457ebdb50c7c9df1173e8f5125f0d
mahoutogroup <mahoutox@gmail.com>
2026-07-21 19:05:26 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,3 +7,17 @@ const supabase = window.supabase.createClient(
 // Vérification de la connexion
 console.log("MAHOUTO+ connecté à Supabase !");
 console.log("URL :", window.MAHOUTO_CONFIG.SUPABASE_URL);
+// Connexion avec Google
+async function loginGoogle() {
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "google",
+    options: {
+      redirectTo: "https://mahouto.vercel.app"
+    }
+  });
+
+  if (error) {
+    console.error("Erreur de connexion Google :", error.message);
+    alert("La connexion Google a échoué.");
+  }
+}
