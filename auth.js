4588cd22b09a8fb558ccd59586bd6ce61741a183
mahoutogroup <mahoutox@gmail.com>
2026-07-21 18:55:03 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,9 @@
+// Initialisation de Supabase
+const supabase = window.supabase.createClient(
+  window.MAHOUTO_CONFIG.SUPABASE_URL,
+  window.MAHOUTO_CONFIG.SUPABASE_ANON_KEY
+);
 
+// Vérification de la connexion
+console.log("MAHOUTO+ connecté à Supabase !");
+console.log("URL :", window.MAHOUTO_CONFIG.SUPABASE_URL);
