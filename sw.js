ff68fdd51abf97c7a92b36857ca886798c9ce075
mahoutogroup <mahoutox@gmail.com>
2026-07-23 14:42:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // network-first pour tout le reste (données dynamiques).
 // =========================================================
 
-const CACHE_NAME = "mahoutoplus-shell-v5";
+const CACHE_NAME = "mahoutoplus-shell-v6";
 
 const APP_SHELL = [
   "./",
