80fc08c5b3426ad5f72cd44ab825d03a514afe66
mahoutogroup <mahoutox@gmail.com>
2026-07-31 08:03:26 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // network-first pour tout le reste (données dynamiques).
 // =========================================================
 
-const CACHE_NAME = "mahoutoplus-shell-v25";
+const CACHE_NAME = "mahoutoplus-shell-v27";
 
 const APP_SHELL = [
   "./",
